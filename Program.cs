ebab36d6eea6a5218985a69791d4c18c1ffe1698
lethimaianh.sut@gmail.com <lethimaianh.sut@gmail.com>
2020-12-12 18:42:48 +0700

    code2

--- Program.cs
+++ Program.cs
@@ -14,16 +14,20 @@ namespace GAS
             do
             {
                 Console.Clear();
-                Console.WriteLine("\t\t\t\t************************************\t\t\t");
-                Console.WriteLine("\t\t\t\t*           QUAN LY BAN GAS        *\t\t\t");
-                Console.WriteLine("\t\t\t\t*        1.Quan ly hang hoa        *\t\t\t");
-                Console.WriteLine("\t\t\t\t*        2.Quan ly nha cung cap     *\t\t\t");
-                Console.WriteLine("\t\t\t\t*        3.Quan ly nhan vien        *\t\t\t");
-                Console.WriteLine("\t\t\t\t*        4.Quan ly hoa don ban      *\t\t\t");
-                Console.WriteLine("\t\t\t\t*        5.Quan ly hoa don nhap     *\t\t\t");
-                Console.WriteLine("\t\t\t\t*        6.Ket thuc                *\t\t\t");
-                Console.WriteLine("\t\t\t\t************************************\t\t\t");
-                Console.WriteLine("Nhap n");
+                Console.WriteLine("");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("\t\t\t\t************************************************************\t\t\t");
+                Console.WriteLine("\t\t\t\t*                     QUAN LY BAN GAS                      *\t\t\t");
+                Console.WriteLine("\t\t\t\t*                 1.Quan ly hang hoa                       *\t\t\t");
+                Console.WriteLine("\t\t\t\t*                 2.Quan ly nha cung cap                   *\t\t\t");
+                Console.WriteLine("\t\t\t\t*                 3.Quan ly nhan vien                      *\t\t\t");
+                Console.WriteLine("\t\t\t\t*                 4.Quan ly hoa don nhap                   *\t\t\t");
+                Console.WriteLine("\t\t\t\t*                 5.Quan ly hoa don ban                    *\t\t\t");
+                Console.WriteLine("\t\t\t\t*                 6.Ket thuc                               *\t\t\t");
+                Console.WriteLine("\t\t\t\t************************************************************\t\t\t");
+                Console.WriteLine("Chon thong tin:");
                 int n = int.Parse(Console.ReadLine());
                 switch (n)
                 {
@@ -40,7 +44,18 @@ namespace GAS
                         frm3.Menu();
                         break;
                     case 4:
-                        FormHDB frm4 = new FormHDB();
+                        FormHDN frm4 = new FormHDN();
+                        frm4.Menu();
+                        break;
+
+                    case 5:
+                        FormHDB frm5 = new FormHDB();
+                        frm5.Menu();
+                        break;
+                    case 6:
+                        break;
+                        
+                   
                 }
             } while (true);
         }
